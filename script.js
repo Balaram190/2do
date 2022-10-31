993ca800faced4db42e02b3b8b964205c216b419
Balaram190 <115470899+Balaram190@users.noreply.github.com>
2022-10-31 09:58:43 +0545

    Update script.js

--- script.js
+++ script.js
@@ -21,45 +21,45 @@ window.addEventListener("load", () => {
             buttonDiv.setAttribute("id", "edit_delete");
             task.appendChild(buttonDiv);
 
-            let edit = document.createElement("button");
-            edit.innerText = "Edit";
-            edit.setAttribute("id", "edit");
-            buttonDiv.appendChild(edit);
+//             let edit = document.createElement("button");
+//             edit.innerText = "Edit";
+//             edit.setAttribute("id", "edit");
+//             buttonDiv.appendChild(edit);
 
-            let remove = document.createElement("button");
-            remove.innerText = "Delete";
-            remove.setAttribute("id", "delete");
-            buttonDiv.appendChild(remove);
+//             let remove = document.createElement("button");
+//             remove.innerText = "Delete";
+//             remove.setAttribute("id", "delete");
+//             buttonDiv.appendChild(remove);
 
-            input.value = "";
+//             input.value = "";
 
-            remove.addEventListener("click", () => {
-
-
-                addedTask.removeChild(task);
-
-            });
+//             remove.addEventListener("click", () => {
 
 
-            edit.addEventListener("click", () => {
-                let edit = prompt("Edited task here");
+//                 addedTask.removeChild(task);
 
-                console.log(edit);
-                if (edit == null) {
-                    return;
-                }
-                else {
-                    para.innerText = edit;
-                }
+//             });
 
 
-            });
-        } else {
-            alert("please enter your task");
-        }
+//             edit.addEventListener("click", () => {
+//                 let edit = prompt("Edited task here");
+
+//                 console.log(edit);
+//                 if (edit == null) {
+//                     return;
+//                 }
+//                 else {
+//                     para.innerText = edit;
+//                 }
+
+
+//             });
+//         } else {
+//             alert("please enter your task");
+//         }
     });
 
 })
-function removeAll() {
-    document.getElementById("task-list").innerHTML = "";
-}
+// function removeAll() {
+//     document.getElementById("task-list").innerHTML = "";
+// }
